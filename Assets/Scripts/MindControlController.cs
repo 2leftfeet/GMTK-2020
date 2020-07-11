aa9420e1be7a9785bd36956d4f9c44e6a93bd232
Liutauras <liutauras@unity3d.com>
2020-07-11 16:24:16 +0300

    Initial mind control API stuff

---
+++ Assets/Scripts/MindControlController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public interface IAgentInput
+{
+    Vector3 TargetPos{ get;}
+    void ResetTargetPos(Vector3 basePos);
+}
+
+public class MindControlController : MonoBehaviour
+{
+    [SerializeField] Controllable startAgent;
+    [SerializeField] IAgentInput input;
+
+    Controllable currentAgent;
+    Camera mainCamera;
+
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        input = GetComponent<IAgentInput>();
+
+        currentAgent = startAgent;
+        currentAgent.input = input;
+        input.ResetTargetPos(currentAgent.transform.position);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100)){
+                var potentialAgent = hit.collider.gameObject.GetComponent<Controllable>();
+                if(potentialAgent != null && potentialAgent != currentAgent)
+                {
+                    currentAgent.input = null;
+                    currentAgent = potentialAgent;
+                    currentAgent.input = input;
+                    input.ResetTargetPos(currentAgent.transform.position);
+                }
+            }
+        }
+    }
+}
