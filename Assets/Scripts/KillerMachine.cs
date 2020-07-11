53c4b87489e5ec4974c4c441b839b7b23abc5d28
Rokevicius <rokas@kazlauskas.eu>
2020-07-11 23:43:29 +0300

    Sentry scene + KillerMachine script

---
+++ Assets/Scripts/KillerMachine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerMachine : MonoBehaviour
+{
+    bool isUsed = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isUsed)
+        {
+            Destroy(collision.gameObject);
+            //Sukurt Dissolve Effect
+            isUsed = true;
+        }
+    }
+}
