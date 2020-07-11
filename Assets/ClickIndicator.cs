38c9bc9484a616986810cc2af38e102fa24c9b72
Rokevicius <rokas@kazlauskas.eu>
2020-07-12 01:49:39 +0300

    Added Click Indicator, Level 9,10

---
+++ Assets/ClickIndicator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ClickIndicator : MonoBehaviour
+{
+    [SerializeField] GameObject indicator;
+    NavMeshAgent agent;
+    Vector3 destination;
+    public IAgentInput input;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = agent.destination;
+            SpawnIndicator();
+        }
+    }
+
+    void SpawnIndicator()
+    {
+        if(GameObject.FindGameObjectsWithTag("Indicator").Length == 0)
+        {
+            Instantiate(indicator, destination,Quaternion.identity);
+        }
+        else
+        {
+            Destroy(GameObject.FindGameObjectsWithTag("Indicator")[0]);
+            Instantiate(indicator, destination,Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/DestroyOnCollide.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyOnCollide : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Character")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
