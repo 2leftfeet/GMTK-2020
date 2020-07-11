dda84fe3c24bd3dbe68171bd978799b9b49857db
Rokevicius <rokas@kazlauskas.eu>
2020-07-11 19:13:32 +0300

    Updated Justas scene

---
+++ Assets/Scripts/NullInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NullInput : MonoBehaviour, IAgentInput
+{
+    // Start is called before the first frame update
+    public void DoUpdate(NavMeshAgent agent)
+    {}
+
+    public void Pause(NavMeshAgent agent)
+    {}
+
+    public void Resume()
+    {}
+}
